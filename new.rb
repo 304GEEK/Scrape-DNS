66564ba27de81f2763d29339ecbbd6577a87b9d7
304geek <rob@304geeks.com>
2013-01-11 21:37:39 -0500

    adding all files for v1

--- new.rb
+++
@@ -1,8 +0,0 @@
-use Net::DNS;
-
-my $res = Net::DNS::Resolver(:nameservers => "129.71.1.1",
-                              :recursive => false,
-                              :retry => 10)
-my $query = $res.search("update.symantec.com");
-
-print address
--- new2.rb
+++
@@ -1,7 +0,0 @@
-class Net::DNS
-res = Net::DNS::Resolver.new(:nameservers => "129.71.1.1",
-                               :recursive => false,
-                               :retry => 10);
-my $query = $res.search("host.example.com");
-
-print $rr
