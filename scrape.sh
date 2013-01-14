1f620c0e16a352cced806a5c27b19a05a282bfb4
304GEEK <rob@304geeks.com>
2013-01-14 13:28:07 -0500

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -68,13 +68,10 @@ do
              ;;
      esac
 done
-echo "     	     	     _        _        _        _        _        _    "
-echo "  	   	   _( )__   _( )__   _( )__   _( )__   _( )__   _( )__ "
-echo " 		 	 _|     _|_|     _|_|     _|_|     _|_|     _|_|     _|"
-echo "			(_ S _ (_(_ C _ (_(_ R _ (_(_ A _ (_(_ P _ (_(_ E _ (_ "
-echo "  		  |_( )__| |_( )__| |_( )__| |_( )__| |_( )__| |_( )__|"
+
 echo " "
 echo "                              	Scrape-DNS v1							"
+echo" "
 echo "                  A proof of concept DNS reconnaissance tool using Dig "
 echo "		      to report on suspicious or revealing cached DNS entries.				"
 echo "            Written by Rob Dixon - Senior Security Consultant - AccuvantLABS		"
