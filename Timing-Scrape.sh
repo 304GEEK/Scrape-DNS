65c55802c4c5c4a8a76cce0e262be28de55341ef
304GEEK <rob@304geeks.com>
2014-02-24 16:06:33 -0500

    Update Timing-Scrape.sh

--- Timing-Scrape.sh
+++ Timing-Scrape.sh
@@ -1,3 +1,14 @@
+#########################################################################
+#									#
+#	Copyright (c) 2013 Rob Dixon 					#
+#									#
+# This program is free software; you can redistribute it and/or		#
+# modify it under the terms of the GNU General Public License as	#
+# published by the Free Software Foundation; either version 2 of the	#
+# License, or (at your option) any later version.  See the file		#
+# COPYING included with this distribution for more information.		#							#
+#########################################################################
+
 TARGET=$1
 MODE=$2
 
